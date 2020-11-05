e98f94ef6be98d44d1e109049359a5c30679e010
HitzSPB <daniel.oesterbro@gmail.com>
2020-11-06 00:48:41 +0100

    Moved the data from old js file to the js file received by the project template

--- src/js/script.js
+++ src/js/script.js
@@ -4,25 +4,63 @@ project 1 - A Random Quote Generator
 ******************************************/
 
 // For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+// Check the "Project Resources" section of the project instructions
+// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
  * `quotes` array 
 ***/
-
+const quotes = [
+  {
+    quote: "1I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
+    source: "Marilyn Monroe",
+    date: Date.now()
+  },
+  {
+    quote: "2I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
+    source: "Marilyn Monroe",
+    date: Date.now()
+  },
+  {
+    quote: "3I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
+    source: "Marilyn Monroe",
+    date: Date.now()
+  },
+  {
+    quote: "4I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
+    source: "Marilyn Monroe",
+    date: Date.now()
+  },
+  {
+    quote: "5I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
+    source: "Marilyn Monroe",
+    date: Date.now()
+  },
+  {
+    quote: "6I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
+    source: "Marilyn Monroe",
+    date: Date.now()
+  }
+]
 
 
 /***
  * `getRandomQuote` function
 ***/
+// We accept that the random number can be 0, as it will be used to get a random quote from an array. 
+const getRandomNumber = (maxNumber) => Math.floor(Math.random() * maxNumber);
 
+function getRandomQuote() {
+  let quoteInformation = quotes[getRandomNumber(quotes.length)];
+  let html = `<p>${quoteInformation["quote"]} test</p>`
+  setHtmlMainObject(html);
+}
 
 
 /***
  * `printQuote` function
 ***/
-
+const printQuote = () => document.querySelector("main").insertAdjacentHTML("beforeend", html);
 
 
 /***
