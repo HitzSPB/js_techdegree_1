74b99462155061528db14831e8f91eda6d69a911
HitzSPB <daniel.oesterbro@gmail.com>
2020-11-06 00:44:04 +0100

    Imported the default project files

--- src/js/quotes.js
+++ src/js/quotes.js
@@ -9,7 +9,7 @@ function getRandomQuote() {
 }
 
 
-// Consider moving this to a json file or similar storage.
+// Consider moving this to a json file or a similar solution.
 const quotes = [
     {
         quote: "1I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
